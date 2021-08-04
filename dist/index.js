b52013611d972d05b7a4f562497faeac639d11b4
iiy4383 <znlspf123@naver.com>
2021-08-04 22:15:18 +0900

    nomadcoder course finished

--- dist/index.js
+++ dist/index.js
@@ -10,11 +10,65 @@ class Block {
         this.timestamp = timestamp;
     }
 }
-Block.caculateHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.calculateHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (block) => typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.data === "string" &&
+    typeof block.timestamp === "number"
+    ? true
+    : false;
 const firstBlock = new Block(0, "4q289304yu0", "", "Hello there?", 202008042025);
 let blockchain = [firstBlock];
 const getBlock = () => blockchain;
-const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getLatestBlock = () => {
+    const block = getBlock();
+    return block[block.length - 1];
+};
+const getHash = (block) => Block.calculateHash(block.index, block.previousHash, block.timestamp, block.data);
 const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const isBlockValid = (block, prevBlock) => {
+    if (!Block.validateStructure(block)) {
+        return false;
+    }
+    else if (prevBlock.index + 1 !== block.index) {
+        return false;
+    }
+    else if (prevBlock.hash !== block.previousHash) {
+        return false;
+    }
+    else if (prevBlock.timestamp > block.timestamp &&
+        prevBlock.timestamp !== 202008042025) {
+        return false;
+    }
+    else if (getHash(block) !== block.hash) {
+        return false;
+    }
+    else
+        return true;
+};
+const createBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const index = previousBlock.index + 1;
+    const previousHash = previousBlock.hash;
+    const timestamp = getNewTimestamp();
+    const hash = Block.calculateHash(index, previousHash, timestamp, data);
+    const newBlock = new Block(index, hash, previousHash, data, timestamp);
+    console.log("GO", newBlock, getLatestBlock());
+    return newBlock;
+};
+const addBlock = (block) => {
+    if (isBlockValid(block, getLatestBlock())) {
+        blockchain = [...blockchain, block];
+    }
+    else {
+        console.log("Invalid block has been input.");
+        return false;
+    }
+};
+addBlock(createBlock("Hey"));
+addBlock(createBlock("Bye"));
+addBlock(createBlock("What's up?"));
+addBlock(createBlock("Is it ok to go?"));
 console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,22 @@ class Block {
   public data: string;
   public timestamp: number;
 
-  static caculateHash = (
+  static calculateHash = (
     index: number,
     previousHash: string,
     timestamp: number,
     data: string
   ): string =>
     CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (block: Block): boolean =>
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.data === "string" &&
+    typeof block.timestamp === "number"
+      ? true
+      : false;
 
   constructor(
     index: number,
@@ -42,8 +51,66 @@ let blockchain: Block[] = [firstBlock];
 
 const getBlock = (): Block[] => blockchain;
 
-const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getLatestBlock = (): Block => {
+  const block = getBlock();
+  return block[block.length - 1];
+};
+
+const getHash = (block: Block): string =>
+  Block.calculateHash(
+    block.index,
+    block.previousHash,
+    block.timestamp,
+    block.data
+  );
 
 const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const isBlockValid = (block: Block, prevBlock: Block): boolean => {
+  if (!Block.validateStructure(block)) {
+    return false;
+  } else if (prevBlock.index + 1 !== block.index) {
+    return false;
+  } else if (prevBlock.hash !== block.previousHash) {
+    return false;
+  } else if (
+    prevBlock.timestamp > block.timestamp &&
+    prevBlock.timestamp !== 202008042025
+  ) {
+    return false;
+  } else if (getHash(block) !== block.hash) {
+    return false;
+  } else return true;
+};
+
+const createBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const index: number = previousBlock.index + 1;
+  const previousHash: string = previousBlock.hash;
+  const timestamp: number = getNewTimestamp();
+  const hash: string = Block.calculateHash(
+    index,
+    previousHash,
+    timestamp,
+    data
+  );
+  const newBlock: Block = new Block(index, hash, previousHash, data, timestamp);
+  console.log("GO", newBlock, getLatestBlock());
+  return newBlock;
+};
+
+const addBlock = (block: Block): void | boolean => {
+  if (isBlockValid(block, getLatestBlock())) {
+    blockchain = [...blockchain, block];
+  } else {
+    console.log("Invalid block has been input.");
+    return false;
+  }
+};
+
+addBlock(createBlock("Hey"));
+addBlock(createBlock("Bye"));
+addBlock(createBlock("What's up?"));
+addBlock(createBlock("Is it ok to go?"));
 
 console.log(blockchain);
